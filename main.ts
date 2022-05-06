860fe285ec1c11bf1bf6b3edbfc9d108ee18c9b4
ntsens <84364452+ntsens@users.noreply.github.com>
2022-05-06 18:30:58 +0800

    添加机器人画三角形程序

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+writingrobot.autoHome()
+writingrobot.autoHome()
+basic.showIcon(IconNames.SmallHeart)
+for (let index = 0; index < 4; index++) {
+    writingrobot.moveXYZ(70, 70, 20)
+    basic.pause(100)
+    writingrobot.moveXYZ(70, 70, -28)
+    basic.pause(100)
+    writingrobot.moveXYZ(150, 70, -28)
+    basic.pause(100)
+    writingrobot.moveXYZ(90, 150, -28)
+    basic.pause(100)
+    writingrobot.moveXYZ(70, 70, -28)
+    basic.showIcon(IconNames.Heart)
+}
+writingrobot.autoHome()
+basic.forever(function () {
+	
+})
