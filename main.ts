0f0e2afe36e32425f3fd9882a70e3f97d82ce1b9
ntsens <84364452+ntsens@users.noreply.github.com>
2022-05-07 11:16:07 +0800

    优化控制

--- main.ts
+++ main.ts
@@ -1,18 +1,22 @@
 writingrobot.autoHome()
-writingrobot.autoHome()
 basic.showIcon(IconNames.SmallHeart)
+let x_offset = 100
+let y_offset = 100
+basic.pause(100)
 for (let index = 0; index < 4; index++) {
-    writingrobot.moveXYZ(70, 70, 20)
-    basic.pause(100)
-    writingrobot.moveXYZ(70, 70, -28)
-    basic.pause(100)
-    writingrobot.moveXYZ(150, 70, -28)
-    basic.pause(100)
-    writingrobot.moveXYZ(90, 150, -28)
-    basic.pause(100)
-    writingrobot.moveXYZ(70, 70, -28)
+    writingrobot.moveXYZ(x_offset, y_offset, 0)
+    basic.pause(200)
+    writingrobot.moveXYZ(100, 100, -32)
+    basic.pause(200)
+    writingrobot.moveXYZ(180, 100, -32)
+    basic.pause(200)
+    writingrobot.moveXYZ(140, 180, -32)
+    basic.pause(200)
+    writingrobot.moveXYZ(100, 100, -32)
     basic.showIcon(IconNames.Heart)
 }
+// 归位
+writingrobot.moveXYZ(x_offset, y_offset, 0)
 writingrobot.autoHome()
 basic.forever(function () {
 	
