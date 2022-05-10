85b135ffe6710c2b51a4752ae5798c4d16e8d34b
ntsens <84364452+ntsens@users.noreply.github.com>
2022-05-10 18:31:03 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,18 @@ writingrobot.autoHome()
 basic.showIcon(IconNames.SmallHeart)
 let x_offset = 100
 let y_offset = 100
+let z_offset = -42
 basic.pause(100)
 for (let index = 0; index < 4; index++) {
     writingrobot.moveXYZ(x_offset, y_offset, 0)
     basic.pause(200)
-    writingrobot.moveXYZ(100, 100, -42)
+    writingrobot.moveXYZ(100, 100, z_offset)
     basic.pause(200)
-    writingrobot.moveXYZ(180, 100, -42)
+    writingrobot.moveXYZ(180, 100, z_offset)
     basic.pause(200)
-    writingrobot.moveXYZ(140, 180, -42)
+    writingrobot.moveXYZ(140, 180, z_offset)
     basic.pause(200)
-    writingrobot.moveXYZ(100, 100, -42)
+    writingrobot.moveXYZ(100, 100, z_offset)
     basic.showIcon(IconNames.Heart)
 }
 // 归位
